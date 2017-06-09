b0d7c7cf4c7826a04ecb0aae423e07bfba733f6f
Li Xianpeng <131100007@mail.smail.nju.edu.cn>
2017-06-09 22:38:09 +0800

    Upgraded to the latest settings

--- POST-INSTALL/4kfix.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-sudo perl -i.bak -pe 's|\xB8\x01\x00\x00\x00\xF6\xC1\x01\x0F\x85|\x33\xC0\x90\x90\x90\x90\x90\x90\x90\xE9|sg' /System/Library/Frameworks/CoreDisplay.framework/Versions/Current/CoreDisplay 
-sudo codesign -f -s - /System/Library/Frameworks/CoreDisplay.framework/Versions/Current/CoreDisplay
-echo "Now clear the IntelGFX in your Clover config.plist" 
--- POST-INSTALL/kext-cache.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sudo cp ./LE/* /Library/Extensions/
-sudo cp ./SLE/* /System/Library/Extensions/
-sudo rm -rf /System/Library/Caches/com.apple.kext.caches/Startup/kernelcache
-sudo rm -rf /System/Library/PrelinkedKernels/prelinkedkernel
-sudo touch /System/Library/Extensions && sudo kextcache -u /
